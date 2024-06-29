b2c4ee219f88d2289de7a6064c1045b0c25303d4
Carlos Filipe <thecarlosfilipe@gmail.com>
2024-06-28 23:45:55 -0300

    feat: Add READ

--- core/crud.ts
+++ core/crud.ts
@@ -3,11 +3,57 @@ import fs from 'fs'; // ES6
 
 const DB_FILE_PATH = './core/db';
 
-console.log("[CRUD]");
+console.log('[CRUD COM QUALIDADE]');
 
-function createContent(content: string) {
-  fs.writeFileSync(DB_FILE_PATH, content);
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
+}
+
+function create(content: string) {
+  const todo: Todo = {
+    date: new Date().toLocaleString('pt-BR'),
+    content: content,
+    done: false,
+  };
+
+  const todos: Array<Todo> = [...read(), todo];
+
+  console.log(todo);
+
+  // salvar a TODO no sistema
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+
   return content;
 }
 
-console.log(createContent('Criando um CRUD com TS.'));
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8');
+  const db = JSON.parse(dbString || '{}');
+  if (!db.todos) {
+    return [];
+  }
+  return db.todos;
+}
+
+function CLEAR_DB() {
+  fs.writeFileSync(DB_FILE_PATH, '');
+}
+
+CLEAR_DB();
+
+create('Primeira TODO!');
+create('Segunda TODO!');
+create('Terceira TODO!');
+
+console.log(read());
